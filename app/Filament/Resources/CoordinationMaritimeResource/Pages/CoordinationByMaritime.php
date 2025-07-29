590406859459a28031b5f2a8d055790f2408dfd8
Jose Perez <joserph.a@gmail.com>
2025-07-29 14:59:11 -0500

    Agregamos la pagina personalizada de la coordinacion maritima

---
+++ app/Filament/Resources/CoordinationMaritimeResource/Pages/CoordinationByMaritime.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CoordinationMaritimeResource\Pages;
+
+use App\Filament\Resources\CoordinationMaritimeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class CoordinationByMaritime extends ListRecords
+{
+    protected static string $resource = CoordinationMaritimeResource::class;
+}
